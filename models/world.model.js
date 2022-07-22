c8839119479d93a52f638c687b850322e0bcbff9
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-07-22 12:34:04 +0200

    testeando rama predev

--- models/world.model.js
+++ models/world.model.js
@@ -4,6 +4,7 @@ const { Schema, model } = mongoose;
 const characterSchema = new Schema({
     nombre: { type: String, unique: true, required: true },
     personajes: { type: Schema.Types.ObjectId, ref: "Character" },
+//locacion: string
 });
 
 module.exports = model("User", characterSchema);
