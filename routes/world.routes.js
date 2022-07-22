42fc888f03c9975337ca08a042f4f85258a47a78
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-07-22 16:14:05 +0200

    ruta post world terminada

--- routes/world.routes.js
+++ routes/world.routes.js
@@ -3,3 +3,11 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const World = require("../models/World.model")
+
+router.post("/world", (req, res, next)=>{
+    const {nombre, breveDescripcion, rpgSistem} = req.body;
+
+    World.create({nombre, breveDescripcion, rpgSistem})
+    .then((newWorld) => res.json(newWorld))
+    .catch((err) => res.json(err))
+});
